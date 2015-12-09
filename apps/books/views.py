4f1a5f94aa1e2f34750658aca75cce87aacc2629
ChillarAnand <anand21nanda@gmail.com>
2015-12-09 23:06:58 +0530

    Updated search page

--- apps/books/views.py
+++ apps/books/views.py
@@ -3,31 +3,43 @@ import urllib
 from django.db.models import Q
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 
 from .models import Book
+# from .forms import SearchForm
 
 
-def home_view(request):
+@require_http_methods(['GET', 'POST'])
+def home(request):
     """
     Home page for iltr.
     """
     return render(request, 'home.html')
 
 
-def book_or_author(request, query):
-    """
-    Auto complete function for book, author names.
-    """
-    if query:
-        query = urllib.parse.unquote(query)
-        results = Book.objects.filter(
-            Q(name__icontains=query) |
-            Q(author__first_name__icontains=query) |
-            Q(author__middle_name__icontains=query) |
-            Q(author__last_name__icontains=query) 
-        )
-        data = [[i.name, str(i.author)] for i in results[:21]]
-        if data:
-            return JsonResponse(data, safe=False)
+@require_http_methods(['GET', 'POST'])
+def search(request):
+    query = request.GET.get('query', '')
+    return render(request, 'home.html')
+    
 
+# def search(request, query):
+#     """
+#     Auto complete function for book, author names.
+#     """
+#     if query:
+#         query = urllib.parse.unquote(query)
+#         results = Book.objects.filter(
+#             Q(name__icontains=query) |
+#             Q(author__first_name__icontains=query) |
+#             Q(author__middle_name__icontains=query) |
+#             Q(author__last_name__icontains=query) 
+#         )
+#         data = [[i.name, str(i.author)] for i in results[:21]]
+#         if not data:
+#             data = ['Couldn\'t find "{}".'.format(query)]
+#         return JsonResponse(data, safe=False)
+        
+#     else:
+#         return JsonResponse([], safe=False)
 
--- iltr/urls.py
+++ iltr/urls.py
@@ -1,13 +1,24 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from apps.books.views import home_view, book_or_author
+from apps.books.views import home, search
+from django.conf import settings
+
 
 
 urlpatterns = patterns(
     '',
 
-    url(r'^$', home_view, name='home'),
-    url(r'^book_or_author/(?P<query>[\w|\W]+)/$', book_or_author, name='book_or_author'),
+    url(r'^$', home, name='home'),
+    url(r'^search', search, name='search'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                            url(r'^media/(?P<path>.*)$',
+                                'django.views.static.serve',
+                                {'document_root': settings.MEDIA_ROOT,
+                                 }),
+                            )
+    
